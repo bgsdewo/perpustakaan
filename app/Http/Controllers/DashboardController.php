8e22ad463998ae9dc93d249feda01f84ef4f92d5
Bagus Sadewo <bgsdewoo@gmail.com>
2026-07-26 14:37:39 +0700

    dashboard done pt1

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Response;
+
 class DashboardController extends Controller
 {
     public function index(): Response
-    {
-        return inertia('Dashboard');
+    { {
+            return inertia(component: 'Dashboard', props: [
+                'page_settings' => [
+                    'title' => 'Dashboard',
+                    'subtitle' => 'Menampilkan semua statistik pada platform ini.',
+                ],
+
+            ]);
+        }
     }
 }
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,7 +1,188 @@
+import CardStat from '@/Components/CardStat';
+import HeaderTitle from '@/Components/HeaderTitle';
+import { Button } from '@/Components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/Components/ui/card';
+import { Table, TableBody, TableHead, TableHeader, TableRow } from '@/Components/ui/table';
 import AppLayout from '@/Layouts/AppLayout';
+import { Link } from '@inertiajs/react';
+import {
+    IconArrowUpRight,
+    IconBooks,
+    IconCreditCardPay,
+    IconCreditCardRefund,
+    IconDashboard,
+    IconMoneybag,
+    IconUsersGroup,
+} from '@tabler/icons-react';
 
-export default function Dashboard() {
-    return <div>Ini dashboard</div>;
+export default function Dashboard(props) {
+    const auth = props.auth.user;
+    return (
+        <div className="flex w-full flex-col space-y-4 pb-32">
+            <div className="flex flex-col items-start justify-between gap-y-4 lg:flex-row lg:items-center">
+                <HeaderTitle
+                    title={props.page_settings.title}
+                    subtitle={props.page_settings.subtitle}
+                    icon={IconDashboard}
+                />
+            </div>
+            {/* admin */}
+            {auth.role.some((role) => ['admin', 'operator'].includes(role)) && (
+                <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+                    <CardStat
+                        data={{
+                            title: 'Total Buku',
+                            icon: IconBooks,
+                            background: 'text-white bg-gradient-to-r from-blue-400 via-blue-500 to-blue-500',
+                            iconClassName: 'text-white',
+                        }}
+                    >
+                        <div className="text-2xl-font-bold">0</div>
+                    </CardStat>
+                    <CardStat
+                        data={{
+                            title: 'Total Pengguna',
+                            icon: IconUsersGroup,
+                            background: 'text-white bg-gradient-to-r from-purple-400 via-purple-500 to-purple-500',
+                            iconClassName: 'text-white',
+                        }}
+                    >
+                        <div className="text-2xl font-bold">0</div>
+                    </CardStat>
+                    <CardStat
+                        data={{
+                            title: 'Peminjaman',
+                            icon: IconCreditCardPay,
+                            background: 'text-white bg-gradient-to-r from-rose-400 via-rose-500 to-rose-500',
+                            iconClassName: 'text-white',
+                        }}
+                    >
+                        <div className="text-2xl font-bold">0</div>
+                    </CardStat>
+                    <CardStat
+                        data={{
+                            title: 'Pengembalian',
+                            icon: IconCreditCardRefund,
+                            background: 'text-white bg-gradient-to-r from-lime-400 via-lime-500 to-lime-500',
+                            iconClassName: 'text-white',
+                        }}
+                    >
+                        <div className="text-2xl font-bold">0</div>
+                    </CardStat>
+                </div>
+            )}
+
+            {/* member */}
+            {auth.role.some((role) => ['member'].includes(role)) && (
+                <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
+                    <CardStat
+                        data={{
+                            title: 'Total Peminjaman',
+                            icon: IconCreditCardPay,
+                            background: 'text-white bg-gradient-to-r from-blue-400 via-blue-500 to-blue-500',
+                            iconClassName: 'text-white',
+                        }}
+                    >
+                        <div className="text-2xl-font-bold">0</div>
+                    </CardStat>
+                    <CardStat
+                        data={{
+                            title: 'Total Pengembalian',
+                            icon: IconCreditCardRefund,
+                            background: 'text-white bg-gradient-to-r from-purple-400 via-purple-500 to-purple-500',
+                            iconClassName: 'text-white',
+                        }}
+                    >
+                        <div className="text-2xl font-bold">0</div>
+                    </CardStat>
+                    <CardStat
+                        data={{
+                            title: 'Denda',
+                            icon: IconMoneybag,
+                            background: 'text-white bg-gradient-to-r from-rose-400 via-rose-500 to-rose-500',
+                            iconClassName: 'text-white',
+                        }}
+                    >
+                        <div className="text-2xl font-bold">0</div>
+                    </CardStat>
+                </div>
+            )}
+            <div className="flex w-full flex-col justify-between gap-2 lg:flex-row">
+                <Card className="w-full lg:w-1/2">
+                    <CardHeader>
+                        <div className="flex flex-col justify-between gap-y-4 lg:flex-row lg:items-center">
+                            <div className="flex flex-col gap-y-2">
+                                <CardTitle>Transaksi Peminjaman</CardTitle>
+                                <CardDescription>Anda dapat melihat 5 transaksi terakhir peminjaman</CardDescription>
+                            </div>
+                            <Button variant="orange" asChild>
+                                {auth.role.some((role) => ['admin', 'operator'].includes(role)) ? (
+                                    <Link href={route('admin.loans.index')}>
+                                        Lihat Semua
+                                        <IconArrowUpRight className="size-4" />
+                                    </Link>
+                                ) : (
+                                    <Link href="#">
+                                        Lihat Semua
+                                        <IconArrowUpRight className="size-4" />
+                                    </Link>
+                                )}
+                            </Button>
+                        </div>
+                    </CardHeader>
+                    <CardContent className="p-0 [&_td]:whitespace-nowrap [&_td]:px-6 [&_th]:px-6">
+                        <Table className="w-full">
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead>#</TableHead>
+                                    <TableHead>Kode Peminjaman</TableHead>
+                                    <TableHead>Buku</TableHead>
+                                    <TableHead>Member</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody></TableBody>
+                        </Table>
+                    </CardContent>
+                </Card>
+                <Card className="w-full lg:w-1/2">
+                    <CardHeader>
+                        <div className="flex flex-col justify-between gap-y-4 lg:flex-row lg:items-center">
+                            <div className="flex flex-col gap-y-2">
+                                <CardTitle>Transaksi Pengembalian</CardTitle>
+                                <CardDescription>Anda dapat melihat 5 transaksi terakhir pengembalian</CardDescription>
+                            </div>
+                            <Button variant="orange" asChild>
+                                {auth.role.some((role) => ['admin', 'operator'].includes(role)) ? (
+                                    <Link href={route('admin.return-books.index')}>
+                                        Lihat Semua
+                                        <IconArrowUpRight className="size-4" />
+                                    </Link>
+                                ) : (
+                                    <Link href="#">
+                                        Lihat Semua
+                                        <IconArrowUpRight className="size-4" />
+                                    </Link>
+                                )}
+                            </Button>
+                        </div>
+                    </CardHeader>
+                    <CardContent className="p-0 [&_td]:whitespace-nowrap [&_td]:px-6 [&_th]:px-6">
+                        <Table className="w-full">
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead>#</TableHead>
+                                    <TableHead>Kode Pengembalian</TableHead>
+                                    <TableHead>Buku</TableHead>
+                                    <TableHead>Member</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody></TableBody>
+                        </Table>
+                    </CardContent>
+                </Card>
+            </div>
+        </div>
+    );
 }
 
 Dashboard.layout = (page) => <AppLayout children={page} title="Dashboard" />;
